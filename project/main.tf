08d6a78fcc75d7311cd76ca651901fd84580eae2
Vaibhav Upare <133318395+vaibhav0342@users.noreply.github.com>
2024-07-31 01:15:10 +0530

    Update main.tf

--- project/main.tf
+++ project/main.tf
@@ -3,16 +3,16 @@ provider "aws" {
 }
 
 module "ec2_instance-1" {
-  source               = "./modules/ec2_instance"
-  ami_value            = "ami-09d83d8d719da9808"
+  source               = "./module/ec2-instance"
+  ami_value            = "ami-0205b4db1d0c6e915"
   instance_type_value  = "t2.micro"
   tag_id               = "instance-1"
 }
 
 
 module "ec2_instance-2" {
-  source               = "./modules/ec2_instance"
-  ami_value            = "ami-064983766e6ab3419"
+  source               = "./module/ec2-instance"
+  ami_value            = "ami-0574a94188d1b84a1"
   instance_type_value  = "t3.micro"
   tag_id               = "instance-2"
 }
